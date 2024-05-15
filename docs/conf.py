e6fe1cbcab8589b950babcc3779d1041609fa5f4
Ryan M White <8209604+ryanmwhitephd@users.noreply.github.com>
2024-05-15 13:41:10 -0400

    Update conf.py
    Comment out bibtex

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,8 @@ author = 'Ryan White, Mark Kuster'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinxcontrib.bibtex']
-bibtex_bibfiles = ['references.bib']
+# extensions = ['sphinxcontrib.bibtex']
+# bibtex_bibfiles = ['references.bib']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
