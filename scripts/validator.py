a398910bfedb8d611ecd2e1955993011b2261090
Ryan M White <8209604+ryanmwhitephd@users.noreply.github.com>
2025-09-02 12:49:16 -0400

    84 action merge source taxon xml to taxonomy xml (#100)
    * fix xml root element name in source files and scripts
    
    * add changed-files to workflow
    
    * add validator to run on changed source files
    
    * fix path
    
    * add 2s sleep between calls
    
    * add catch for Requests error
    
    * another attempt to handle requests error
    
    * run validation of multiple files from
    
    * add merging step
    
    * fix merging step
    
    * install yq in workflow
    
    * install yq in workflow
    
    * Update validation.yml for merge step
    
    * add step to limit changes to only source or only catalog.
    
    * add commit stage for merged catalog
    
    * add commit stage for merged catalog
    
    * add commit stage for merged catalog
    
    * add commit stage for merged catalog
    
    * remove commit stage for merged catalog
    
    * direct merge to file
    
    * revert direct merge to file

--- scripts/validator.py
+++ scripts/validator.py
@@ -15,11 +15,23 @@ Requires python environment with xmlschema
 
 import xmlschema
 import argparse
+import time
 from pathlib import Path
 
 def validate(*args):
     schema = xmlschema.XMLSchema('MeasurandTaxonomyCatalog.xsd')
     schema.validate(args[0])
+
+def validate_list(*args):
+    schema = xmlschema.XMLSchema('MeasurandTaxonomyCatalog.xsd')
+    
+    lst = open(args[1], 'r+').read().splitlines()
+
+    for name in lst:
+        p = Path(args[0])
+        f = p / name
+        print(f)
+        schema.validate(f)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -31,10 +43,14 @@ if __name__ == '__main__':
             help="Input taxonomy file")
     parser.add_argument('-d', '--dirpath', help="Output directory")
     parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-l', '--list', help="list of filenames")
     
     args = parser.parse_args()
     print(args)
     p = Path(args.dirpath)
-    f = p / args.filename
-    print(f.resolve())
-    validate(f)
+    if args.list is not None:
+        validate_list(args.dirpath, args.list)
+    else:
+        f = p / args.filename
+        print(f.resolve())
+        validate(f)
